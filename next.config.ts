8b2f576ad44f6d8ddb1a6826122da9de25711791
TafitaNiainaperline <tafitaniainaperline@gmail.com>
2026-04-03 16:39:57 +0300

    configure turbopack sass support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackUseBuiltinSass: true,
+  },
 };
 
 export default nextConfig;
