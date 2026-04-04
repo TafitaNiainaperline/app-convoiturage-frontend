55234d981d22b17a0aa6572d25c5c7948da055e6
TafitaNiainaperline <tafitaniainaperline@gmail.com>
2026-04-04 15:03:38 +0300

    add villes list

---
+++ lib/villes.ts
@@ -0,0 +1,10 @@
+export const COORDS_VILLES: Record<string, [number, number]> = {
+  'Antananarivo': [-18.9137, 47.5361],
+  'Tamatave':     [-18.1492, 49.4023],
+  'Fianarantsoa': [-21.4527, 47.0857],
+  'Majunga':      [-15.7167, 46.3167],
+  'Diego-Suarez': [-12.3547, 49.2936],
+  'Tuléar':       [-23.3500, 43.6667],
+  'Antsirabe':    [-19.8659, 47.0333],
+  'Ambositra':    [-20.5333, 47.2500],
+};
